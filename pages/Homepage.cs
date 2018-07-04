635740a45f37e4b828bd6e3de0407fe7749e3072
Evie Skinner <eskinner@spartaglobal.com>
2018-07-04 12:05:50 +0100

    That way produced build errors so put it back to one big method SuccessfulLogin

--- pages/Homepage.cs
+++ pages/Homepage.cs
@@ -16,7 +16,7 @@ namespace csharp_ui_take_two
     public class Homepage
     {
         [Test]
-        public void LaunchBrowser()
+        public void SuccessfulLogin()
         {
             // I think this creates an instance of chrome driver
             ChromeDriverService service = ChromeDriverService.CreateDefaultService(@"C:\Users\TECH-W77\Documents\take_two_csharp_framework-dev\chromedriver_win32", "chromedriver.exe");
@@ -24,41 +24,41 @@ namespace csharp_ui_take_two
             // Launch browser
             IWebDriver driver = new ChromeDriver(service);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMinutes(1);
-        }
+        
 
-        public void GoToHomepage
-        {
-            // Navigate to URL
+        
+            // Navigate to homepage URL
+           
             driver.Navigate().GoToUrl("https://37a4a4e2.ngrok.io/login");
-        }
+        
 
-        public void FillEmailField
-        {
+        //FillEmailField()
+        
             // Csharp needs double quotation marks as a MUST. so change the dbl ones e.g inside an xpath to single ones to make Csharp comfy.
             // Ooh she's a bit of a diva this Csharp!
             driver.FindElement(By.Name("email")).SendKeys("hello");
 
-        }
         
-        public void FillPasswordField
-        {
+        
+       //FillPasswordField()
+        
             // Enter 'testing' in password bar
             driver.FindElement(By.Name("password")).SendKeys("testing");
 
-        }
         
-        public void ClickSubmitButton
-        {
+        
+        //ClickSubmitButton()
+        
             // Click the submit button
             driver.FindElement(By.XPath("/html/body/div/div[2]/div/div/div/form/div[3]/input")).Click();
 
-        }
         
-        public void CloseApplication
-        {
+        
+       // CloseApplication()
+        
             //close the application
             // driver.Quit();
-        }
+        
 
 
 
